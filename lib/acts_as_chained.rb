08172df8e191a5f92328954efefeb60ac37670ba
James Smith <james@floppy.org.uk>
2011-08-19 16:57:52 +0100

    extend chained attribute support and DRY things up a bit

--- lib/acts_as_chained.rb
+++ lib/acts_as_chained.rb
@@ -7,6 +7,7 @@ module ActsAsChained
     
     def acts_as_chained target, options={}
       # Add functionality into class
+      include ActsAsChained::InstanceMethods
       extend ActsAsChained::ClassMethods
       # Set exclusions
       self.chain_exclusions = [:id] + (options[:except] || [])
@@ -21,29 +22,76 @@ module ActsAsChained
       @@ChainExclusions = exclusions 
     end
     
+    def chain_exclusions
+      @@ChainExclusions
+    end
+
     def chain_target=(target)
       @@Target = target 
     end
 
+    def chain_target
+      @@Target
+    end
+
     def define_attribute_methods
-      # Define all attributes, first of all
+      # First - default behaviour
       super
-      # Now add our chained versions
-      (columns_hash.keys.map{|x| x.to_sym} - @@ChainExclusions).each do |attribute| 
-        define_method(attribute.to_s + '_with_chain') do
-          logger.info "fetching " + attribute.to_s + ' from ' + category
-          val = send(attribute.to_s + '_without_chain')
-          logger.info "--#{category}: " + val.to_yaml
-          if val.nil? && self.send(@@Target)
-            logger.info "--#{category}: chaining!"
-            val = self.send(@@Target).send(attribute)
-            logger.info "--#{category}: " + val.to_yaml
-          end
-          logger.info "--#{category}: done"
-          val
+      # Then, for each non-excluded attribute
+      (columns_hash.keys.map{|x| x.to_sym} - self.chain_exclusions).each do |attribute| 
+        
+        # Redefine accessor method
+        define_method("#{attribute}_with_chain") do
+          chain_impl(attribute, attribute)
         end
         alias_method_chain attribute, :chain
+
+        # Define before type cast accessor methods
+        #define_method("#{attribute}_before_type_cast_with_chain") do
+        #  chain_impl(attribute, "#{attribute}_before_type_cast")
+        #end
+        #alias_method_chain "#{attribute}_before_type_cast", :chain
+
+      end 
+    end
+  end
+    
+  module InstanceMethods
+  
+    def chain_impl(attribute, method_name, options = {})
+      logger.info "calling #{method_name}from #{category} #{options[:argument]}"
+      if options[:argument]
+        val = send "#{method_name}_without_chain", options[:argument]
+      else
+        val = send "#{method_name}_without_chain"
       end
+      logger.info "--#{category}: #{val.to_yaml}"
+      if val.nil? && !self.class.chain_exclusions.include?(attribute) && send(self.class.chain_target)
+        logger.info "--#{category}: chaining!"
+        if options[:argument]
+          val = send(self.class.chain_target).send(method_name, options[:argument])
+        else
+          val = send(self.class.chain_target).send(method_name)
+        end
+        logger.info "--#{category}: #{val.to_yaml}"
+      end
+      logger.info "--#{category}: done"
+      val
+    end
+
+    # Chain general read_attribute in case people use it
+    def read_attribute_with_chain(attribute)
+      chain_impl(attribute, :read_attribute, :argument => attribute)
+    end
+    
+    # Chain query attribute method. Implements attribute? methods
+    def query_attribute_with_chain(attribute)
+      chain_impl(attribute, :query_attribute, :argument => attribute)
+    end
+
+    def self.included(base)
+      base.alias_method_chain :read_attribute, :chain
+      base.alias_method_chain :query_attribute, :chain
     end
     
   end
